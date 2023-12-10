7b6f90e10e69e904543e1838f406c187c22bcf78
lindorapel <71240067+lindorapel@users.noreply.github.com>
2023-12-10 20:15:28 +0700

    Reset password (#24)
    * add getuser api
    
    * resolve eror
    
    * add detail page & detail content
    
    * edit detail page
    
    ---------
    
    Co-authored-by: Arwin Marinta <89586510+ArwinMarinta@users.noreply.github.com>

---
+++ src/pages/DetailPage/DetailPage/DetailPage.jsx
@@ -0,0 +1,32 @@
+import Header from "../../../components/Navbar/Header";
+import Desktop from "../Desktop";
+import Mobile from "../Mobile";
+import { useSelector, useDispatch } from "react-redux";
+import { getCourseDetail } from "../../../redux/actions/DetailActions";
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+
+const DetailPage = () => {
+  const dispatch = useDispatch();
+  const { courseDetail } = useSelector((state) => state.detail) || {};
+  const { user } = useSelector((state) => state.auth);
+
+  const { courseId } = useParams();
+
+  useEffect(() => {
+    dispatch(getCourseDetail(courseId));
+  }, [dispatch, courseId]);
+
+  // useEffect(() => {
+  // }, [dispatch, courseId]);
+
+  return (
+    <>
+      <Header />
+      <Desktop courseDetail={courseDetail} user={user} />
+      <Mobile courseDetail={courseDetail} />
+    </>
+  );
+};
+
+export default DetailPage;
