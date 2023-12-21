de989ba942f9c966a3ca47a17ccad61ba9e6e9c4
AndreAditya <adityaandre0301@gmail.com>
2023-12-21 17:57:10 +0800

    add loading auth

---
+++ src/components/Protecd/NoAccesToken.jsx
@@ -0,0 +1,19 @@
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
+const NoAccesToken = ({ children }) => {
+  const navigate = useNavigate();
+  const { token } = useSelector((state) => state.auth);
+  //Menggunakan useSelector untuk mendapatkan nilai token dari state Redux di atas properti "auth".
+
+  useEffect(() => {
+    if (!token) {
+      navigate("/login");
+    }
+  }, [token, navigate]);
+
+  return children;
+};
+
+export default NoAccesToken;
