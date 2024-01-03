21e4fb57db49eb0d7e23fe1952c406b099e57003
Fatwa Akbar Jiwani <140494742+Fatwaakbarjiwani@users.noreply.github.com>
2024-01-03 18:10:29 +0700

    Solfe (#49)
    * create searchPage
    
    * create searchPage
    
    * create solfe
    
    * fiks
    
    * add discussion
    
    * add discussion
    
    * add discussion
    
    * add discussion
    
    * solf bug in discussion and commentar

---
+++ src/pages/DetailPage/Desktop.jsx
@@ -0,0 +1,199 @@
+import PropTypes from "prop-types";
+import { Link, useNavigate } from "react-router-dom";
+import { FaArrowLeft } from "react-icons/fa";
+import { RiShieldStarLine } from "react-icons/ri";
+import { RiBook2Line } from "react-icons/ri";
+import { RiTimeFill } from "react-icons/ri";
+import { BiLink, BiLogoTelegram} from "react-icons/bi";
+import { FaCirclePlay } from "react-icons/fa6";
+import { BiSolidLock } from "react-icons/bi";
+
+const Desktop = ({ courseDetail, contentDetail, user }) => {
+  const navigate = useNavigate();
+
+  const handleLinkClick = (courseId, moduleId, contentId) => {
+    navigate(
+      `/detail/course/${courseId}/module/${moduleId}/content/${contentId}`
+    );
+  };
+
+  return (
+    <>
+      <div className="hidden sm:block">
+        <div className="bg-DARKBLUE04">
+          <div className="container mx-auto py-5">
+            <Link to="/course" className="flex items-center w-max">
+              <FaArrowLeft />
+              <span className="font-bold pl-2.5">Kelas Lainnya</span>
+            </Link>
+            <div className="pt-5 pb-3">
+              <h1 className="text-xl font-bold text-DARKBLUE05">
+                {courseDetail?.category}
+              </h1>
+              <h2 className="text-lg font-bold">{courseDetail?.title}</h2>
+              <span className="text-sm font-medium">
+                by {courseDetail?.instructor}
+              </span>
+              <div className="flex pt-2 gap-10">
+                <div className="flex items-center  font-medium">
+                  <RiShieldStarLine className="text-lg text-ALERTGREEN" />
+                  <span className="pl-1 text-sm text-DARKBLUE05">
+                    {courseDetail?.level} Level
+                  </span>
+                </div>
+                <div className="flex items-center  font-medium">
+                  <RiBook2Line className="text-lg text-ALERTGREEN" />
+                  <span className="pl-1 text-sm">
+                    {courseDetail?.totalModule} modul
+                  </span>
+                </div>
+                <div className="flex items-center  font-medium">
+                  <RiTimeFill className="text-lg text-ALERTGREEN" />
+                  <span className="pl-1 text-sm">
+                    {courseDetail?.duration} menit
+                  </span>
+                </div>
+              </div>
+            </div>
+            <Link
+              to={courseDetail?.groupDiscussion}
+              className="flex gap-2 rounded-full text-white items-center bg-ALERTGREEN w-max py-1.5 px-8"
+            >
+              <span className="font-bold">Join Group Telegram</span>
+              <BiLogoTelegram className="text-xl " />
+            </Link>
+            <Link to={`/discussion-course/${courseDetail?.courseId}`}>
+              <button className="flex gap-14 rounded-full text-white items-center bg-gray-400 my-2 w-max py-1.5 px-8">
+                <span className="font-bold">Forum Diskusi</span>
+                <BiLink className="text-xl " />
+              </button>
+            </Link>
+          </div>
+        </div>
+
+        <div className="container flex gap-10 mx-auto my-8">
+          <div className="" style={{ flex: "2" }}>
+            <iframe
+              className="w-full aspect-video rounded-2xl bg-black"
+              src={`https://www.youtube.com/embed/${contentDetail?.videoUrl}`}
+              title="YouTube video player"
+              // frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allowfullscreen
+            ></iframe>
+            <div>
+              <h3 className="text-xl font-medium mt-8">Tentang Kelas</h3>
+              <p className="indent-8">{courseDetail?.description}</p>
+            </div>
+            <div>
+              <h3 className="text-xl font-medium mt-8 mb-2.5">
+                Kelas ini ditujukan untuk
+              </h3>
+              <ul className="list-decimal list-inside">
+                {courseDetail?.requirements?.map((requirement, index) => (
+                  <li key={index} className="pl-2 py-1">
+                    {requirement?.requirements}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+          <div
+            className="border shadow-xl drop-shadow-2xl ring-offset-1 border-gray-400/20 rounded-xl h-max"
+            style={{ flex: "1" }}
+          >
+            <div className="py-5 px-6">
+              <h3 className=" font-bold">Materi Belajar</h3>
+              <div className="pt-2.5">
+                {courseDetail.modules?.map((module, moduleIndex) => (
+                  <>
+                    <div className="flex gap-4 justify-between text-sm w-full mt-5 mb-3">
+                      <span className="text-DARKBLUE05 font-bold ">
+                        Chapter {moduleIndex + 1} - {module?.title}
+                      </span>
+                      <span className="font-bold text-DARKBLUE03 whitespace-nowrap">
+                        {module?.duration} menit
+                      </span>
+                    </div>
+                    {courseDetail?.modules?.[moduleIndex]?.contents?.map(
+                      (content, contentIndex) => (
+                        <div className="" key={contentIndex}>
+                          <button
+                            onClick={() =>
+                              handleLinkClick(
+                                courseDetail?.courseId,
+                                module?.moduleId,
+                                content?.contentId
+                              )
+                            }
+                            className="flex items-center justify-between py-1 w-full"
+                          >
+                            <div className="flex items-center text-sm">
+                              <span className="rounded-full bg-DARKBLUE04 text-sm font-medium py-1 px-3 mr-2.5">
+                                {contentIndex + 1}
+                              </span>
+                              <span className=" font-medium">
+                                {content?.title}
+                              </span>
+                            </div>
+                            <span className="text-xl">
+                              {content?.isDemo === true ? (
+                                <FaCirclePlay
+                                  className={`${
+                                    user ? "text-ALERTGREEN" : "text-NEUTRAL02"
+                                  }`}
+                                />
+                              ) : (
+                                <BiSolidLock className="text-black" />
+                              )}
+                            </span>
+                          </button>
+                          <hr className="my-1" />
+                        </div>
+                      )
+                    )}
+                  </>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+Desktop.propTypes = {
+  courseDetail: PropTypes.shape({
+    title: PropTypes.string,
+    description: PropTypes.string,
+    instructor: PropTypes.string,
+    level: PropTypes.string,
+    totalModule: PropTypes.number,
+    duration: PropTypes.number,
+    courseId: PropTypes.number,
+    requirements: PropTypes.string,
+    category: PropTypes.string,
+    groupDiscussion: PropTypes.string,
+    modules: PropTypes.arrayOf(
+      PropTypes.shape({
+        title: PropTypes.string,
+        duration: PropTypes.number,
+        contents: PropTypes.arrayOf(
+          PropTypes.shape({
+            title: PropTypes.string,
+            isDemo: PropTypes.bool,
+            contentId: PropTypes.number, // menambahkan contentId
+            // Tambahkan prop lainnya sesuai kebutuhan
+          })
+        ),
+      })
+    ),
+  }),
+  contentDetail: PropTypes.shape({
+    videoUrl: PropTypes.string, // Mengubah tipe prop videoUrl menjadi string
+  }),
+  user: PropTypes.bool, // menambahkan prop user dengan tipe boolean
+};
+
+export default Desktop;
--- src/pages/DiscussionPage/DetailDiscussion.jsx
+++ src/pages/DiscussionPage/DetailDiscussion.jsx
@@ -1,6 +1,5 @@
 import { useDispatch, useSelector } from "react-redux";
 import chat from "../../assets/chating.svg";
-import user from "../../assets/user.svg";
 import galery from "../../assets/galery.svg";
 import { useEffect, useState } from "react";
 import {
@@ -13,6 +12,7 @@ export default function DetailDiscussion() {
   const { getData } = useSelector((state) => state.course);
   const { detailDiscussion } = useSelector((state) => state.course);
   const { comentar } = useSelector((state) => state.course);
+  const { user } = useSelector((state) => state.auth);
   const dispatch = useDispatch();
   const { id } = useParams();
   const { discussionId } = useParams();
@@ -20,20 +20,23 @@ export default function DetailDiscussion() {
   const [image, setImage] = useState(null);
   const [hasil, setHasil] = useState(null);
   const [jawaban, setJawaban] = useState("");
-  // const [save, setSave] = useState(false);
+  const [errors, setErrors] = useState("");
 
   const handleSave = (e) => {
     e.preventDefault();
-    if (jawaban != "") {
+    if (jawaban != "" && image !== null) {
       dispatch(addComment(jawaban, image, id, discussionId));
+    }
+    if (jawaban == "" && image == null) {
+      setErrors("Jawaban atau gambar tidak boleh kosong");
+      setTimeout(() => {
+        setErrors("");
+      }, 2000);
     }
   };
   const handleImageChange = (e) => {
     const file = e.target.files[0];
     setImage(file);
-    // Validasi atau manipulasi gambar sesuai kebutuhan
-
-    // Set file gambar
     setHasil(URL.createObjectURL(file));
   };
 
@@ -41,13 +44,6 @@ export default function DetailDiscussion() {
     if (id) {
       dispatch(getDetailDiscussion(id, discussionId));
     }
-    // if (save == true) {
-    //   dispatch(addComment(jawaban, image, id, discussionId));
-    //   setJawaban("");
-    //   setImage(null);
-    //   setHasil(null);
-    //   setSave(false);
-    // }
   }, [dispatch, discussionId, id]);
   return (
     <div>
@@ -76,8 +72,8 @@ export default function DetailDiscussion() {
             <div className="flex justify-between">
               <div className="flex flex-row items-center gap-3">
                 <img
-                  src={user}
-                  className="w-10 rounded-full bg-blue-200"
+                  src={detailDiscussion.userPhoto}
+                  className="w-10 rounded-full bg-blue-200 object-top object-cover h-10"
                   alt=""
                 />
                 <h3 className="font-semibold text-xl">
@@ -117,61 +113,79 @@ export default function DetailDiscussion() {
       </div>
       <div className="my-5 border-b-2 border-gray-200"></div>
       <div className="flex justify-center">
-        <div className="container">
-          <div className="flex flex-row items-center gap-3">
-            <img src={user} className="w-10 rounded-full bg-blue-200" alt="" />
-            <h3 className="font-semibold text-xl">Nama Akun</h3>
+        {detailDiscussion.closed == true && (
+          <div className="font-bold text-lg text-gray-400">
+            Comentar ditutup
           </div>
-          <div>
-            <div
-              action=""
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mt-2"
-            >
-              <label htmlFor="imageInput" className="cursor-pointer ">
-                <div className="flex flex-row items-center">
-                  <img src={galery} className="w-8" alt="Select from Gallery" />
-                  <p className="text-gray-400 font-semibold">
-                    Tambahkan Gambar
-                  </p>
-                </div>
-                <input
-                  type="file"
-                  accept="image/*"
-                  id="imageInput"
-                  ref={img}
-                  onChange={handleImageChange}
-                  className="hidden"
-                />
-              </label>
-              {hasil && (
-                <div className="mb-4">
-                  <img
-                    src={hasil}
-                    alt="Preview"
-                    className="w-[25%] h-auto rounded-lg"
-                  />
-                </div>
-              )}
-              <input
-                type="text"
-                id=""
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mt-2"
-                placeholder="Type Something"
-                value={jawaban}
-                onChange={(e) => setJawaban(e.target.value)}
+        )}
+        {detailDiscussion.closed == false && (
+          <div className="container">
+            <div className="flex flex-row items-center gap-3">
+              <img
+                src={user?.photoProfile}
+                className="w-10 rounded-full bg-blue-200 object-top object-cover h-10"
+                alt=""
               />
-              <div className="flex items-center justify-between mt-2">
-                <button
-                  type="submit"
-                  className="bg-gray-600 hover:bg-gray-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                  onClick={handleSave}
-                >
-                  Submit
-                </button>
+              <h3 className="font-semibold text-xl">{user?.name}</h3>
+            </div>
+            <div>
+              <div
+                action=""
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mt-2"
+              >
+                <label htmlFor="imageInput" className="cursor-pointer ">
+                  <div className="flex flex-row items-center">
+                    <img
+                      src={galery}
+                      className="w-8"
+                      alt="Select from Gallery"
+                    />
+                    <p className="text-gray-400 font-semibold">
+                      Tambahkan Gambar (png,jpg,svg...)
+                    </p>
+                  </div>
+                  <input
+                    type="file"
+                    accept="image/*"
+                    id="imageInput"
+                    ref={img}
+                    onChange={handleImageChange}
+                    className="hidden"
+                  />
+                </label>
+                {hasil && (
+                  <div className="mb-4">
+                    <img
+                      src={hasil}
+                      alt="Preview"
+                      className="w-[25%] h-auto rounded-lg"
+                    />
+                  </div>
+                )}
+                <input
+                  type="text"
+                  id=""
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mt-2"
+                  placeholder="Type Something"
+                  value={jawaban}
+                  onChange={(e) => setJawaban(e.target.value)}
+                />
+                {jawaban == "" && (
+                  <p className="text-red-500 font-bold my-2 p-1">{errors}</p>
+                )}
+                <div className="flex items-center justify-between mt-2">
+                  <button
+                    type="submit"
+                    className="bg-gray-600 hover:bg-gray-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                    onClick={handleSave}
+                  >
+                    Submit
+                  </button>
+                </div>
               </div>
             </div>
           </div>
-        </div>
+        )}
       </div>
       <div className="my-5 border-b-2 border-gray-200"></div>
       <div className="flex justify-center">
